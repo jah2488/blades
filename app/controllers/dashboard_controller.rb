39519c32dc4f8cf32411ea1fce91a119bf1a3368
Justin Herrick <justin@lunarcollective.co>
2017-03-11 09:41:38 -0600

    Adding current game to dash.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,7 @@ class DashboardController < ApplicationController
   def index
     render locals: {
       my_games: current_user.games,
-      playing_games: current_user.playing_games
+      playing_games: Array(current_user.game)
     }
   end
 end
